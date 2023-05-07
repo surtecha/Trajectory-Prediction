034d9e3963a98199e968e9c6a0ceb3a83b2e6e90
suryateja-challa <challasuryateja29@gmail.com>
2023-05-07 23:27:52 +0530

    Added status for 'basket' or 'no basket'

--- main.py
+++ main.py
@@ -1,25 +1,30 @@
+import math
 import cv2
 import cvzone
 import numpy as np
 from cvzone.ColorModule import ColorFinder
 
-cap = cv2.VideoCapture('Videos/vid2.mp4')
 
+cap = cv2.VideoCapture('Videos/vid4.mp4')
+
+# Color Finder
 myColorFinder = ColorFinder(False)
 hsvVals = {'hmin': 8, 'smin': 96, 'vmin': 115, 'hmax': 14, 'smax': 255, 'vmax': 255}
 
+# Variables
 posListX, posListY = [], []
 xList = [item for item in range(0, 1300)]
+prediction = False
 
 while True:
+
     success, img = cap.read()
-    #img = cv2.imread("Ball.png")
+    # img = cv2.imread("Ball.png")
     img = img[0:900, :]
 
-    #Find ball color
+    # Finding Ball Color
     imgColor, mask = myColorFinder.update(img, hsvVals)
-
-    #Find ball location
+    # Finding Ball Location
     imgContours, contours = cvzone.findContours(img, mask, minArea=500)
 
     if contours:
@@ -27,27 +32,40 @@ while True:
         posListY.append(contours[0]['center'][1])
 
     if posListX:
-        # Poly Regression: y = Ax^2 + Bx + c
-        # Finding coefficients
+
+        # Polynomial Regression y = Ax^2 + Bx + C
         A, B, C = np.polyfit(posListX, posListY, 2)
 
         for i, (posX, posY) in enumerate(zip(posListX, posListY)):
             pos = (posX, posY)
             cv2.circle(imgContours, pos, 10, (0, 255, 0), cv2.FILLED)
-            if i==0:
+            if i == 0:
                 cv2.line(imgContours, pos, pos, (0, 255, 0), 5)
             else:
-                cv2.line(imgContours, pos, (posListX[i-1], posListY[i-1]), (0, 255, 0), 5)
+                cv2.line(imgContours, pos, (posListX[i - 1], posListY[i - 1]), (0, 255, 0), 5)
 
         for x in xList:
-            y = int(A*x**2 + B*x + C)
+            y = int(A * x ** 2 + B * x + C)
             cv2.circle(imgContours, (x, y), 2, (255, 0, 255), cv2.FILLED)
 
+        if len(posListX) < 10:
+            # Prediction
+            a = A
+            b = B
+            c = C - 590
 
-    # img = cv2.resize(img, (0, 0), fx=0.6, fy=0.6)
-    # imgColor = cv2.resize(imgColor, (0, 0), fx=0.6, fy=0.6)
-    imgContours = cv2.resize(imgContours, (0, 0), fx=0.6, fy=0.6)
+            x = int((-b - math.sqrt(b ** 2 - (4 * a * c))) / (2 * a))
+            prediction = 330 < x < 430
 
+        if prediction:
+            cvzone.putTextRect(imgContours, "Basket", (50, 150),
+                               scale=5, thickness=5, colorR=(0, 200, 0), offset=20)
+        else:
+            cvzone.putTextRect(imgContours, "No Basket", (50, 150),
+                               scale=5, thickness=5, colorR=(0, 0, 200), offset=20)
+
+
+    imgContours = cv2.resize(imgContours, (0, 0), None, 0.7, 0.7)
     # cv2.imshow("Image", img)
-    cv2.imshow("Image Color", imgContours)
-    cv2.waitKey(70)
+    cv2.imshow("ImageColor", imgContours)
+    cv2.waitKey(100)
